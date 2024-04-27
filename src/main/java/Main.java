559f05be9650322c4d021e3b3a90dc79659b78c5
MichalK24 <162459522+MichalK24@users.noreply.github.com>
2024-04-27 10:46:42 +0000

    40-2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,56 +1,35 @@
-/*
-Kod bazowy programu Commit4_0: 
-• Program dodaje do prostej bazy danych (pliku db.txt) dane odnośnie Studentów.
-• Studenci dodawani są w klasie Main.
-• Wszyscy studenci są wypisywani na końcu klasy Main.
-• Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
-• Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
-*/
-import java.util.Scanner;
 import java.io.IOException;
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
       Scanner scanner = new Scanner(System.in);
-      int wybor = 0;
 
-      while(wybor != 3) {
-        System.out.println("1. Dodaj studenta");
-        System.out.println("2. Wyświetl studenta");
-        System.out.println("3. Wyjście");
+      System.out.println("Wybierz opcje:");
+      System.out.println("1 - Dodaj");
+      System.out.println("2 - Wypisz studentów");
+      System.out.println("3 - Wyjście");
 
-        wybor = scanner.nextInt();
-        switch(wybor) {
-          case 1: {
-            System.out.println("Podaj imię");
-            String imie = scanner.next();
-            System.out.println("Podaj wiek");
-            int wiek = scanner.nextInt();
-            s.addStudent(new Student(imie, wiek));
-            break;
-          }
-          case 2: {
-            var students = s.getStudents();
-            for(Student current : students) {
-              System.out.println(current.toString());
-            }
-            break;
-          }
-          case 3: {
-            System.out.println("Wyjście");
-            break;
-          }
-          default: {
-            System.out.println("Niepoprawny wybór");
-            break;
-          }
-        }
+      int choice = scanner.nextInt();
+      switch (choice) {
+        case 1:
+          System.out.print("Imie: ");
+          String name = scanner.next();
+          System.out.print("Wiek: ");
+          int age = scanner.nextInt();
+          s.addStudent(new Student(name, age));
+          break;
+        case 2:
+          System.out.println("Wyjście z programu.");
+          System.exit(0);
+          break;
+        default:
+          System.out.println("Zły wybór.");
       }
     } catch (IOException e) {
-      e.printStackTrace();
+
     }
   }
 }
