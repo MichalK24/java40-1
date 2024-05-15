71bfdaffc1961c08b3e17c9c3fe2975af730df6a
MichalK24 <162459522+MichalK24@users.noreply.github.com>
2024-05-15 12:50:38 +0000

    4-3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,42 @@
 import java.io.IOException;
 import java.util.Scanner;
-
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      Scanner scanner = new Scanner(System.in);
+       Scanner input = new Scanner(System.in);
+      int wybor = 0;
+      while (wybor != 3) {
+      System.out.println("Menu");
+      System.out.println("1. Dodaj studenta");
+      System.out.println("2. Wyświetl wszystkich studentów");
+      System.out.println("3. Zamknięcie programu");
 
-      System.out.println("wybierz opcje:");
-      System.out.println("1 - dodaj");
-      System.out.println("2 - wypisz studentów");
-      System.out.println("3 - wyjście");
+      wybor = input.nextInt();
 
-      int choice = scanner.nextInt();
-      switch (choice) {
-        case 1:
-          System.out.print("imie: ");
-          String name = scanner.next();
-          System.out.print("wiek: ");
-          int age = scanner.nextInt();
-          s.addStudent(new Student(name, age));
-          break;
-        case 2:
-          System.out.println("wyjście z programu.");
-          System.exit(0);
-          break;
-        default:
-          System.out.println("błędny wybór.");
+          switch(wybor) {
+            case 1:
+              System.out.print("Podaj Imie Studenta: ");
+              String name = input.next();
+              System.out.print("Podaj Nazwisko Studenta: ");
+              String nazwisko = input.next();
+              System.out.print("Podaj Wiek Studenta: ");
+              int age = input.nextInt();
+              s.addStudent(new Student(name, nazwisko, age));
+              break;
+
+            case 2:
+              var students = s.getStudents();
+              for(Student current : students) {
+                System.out.println(current.ToString());
+              }
+              break;
+            case 3:
+              break;
+          }
       }
-    } catch (IOException e) {
 
+    } catch (IOException e) {
     }
   }
 }
